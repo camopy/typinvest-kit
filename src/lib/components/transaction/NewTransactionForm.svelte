e64bfdb4abe9553bf6c6c2cceffc4619619892d3
Paulo Camopy <paulorenato.cpb@gmail.com>
2021-04-25 14:23:12 -0300

    feat: :sparkles: added new transaction component

---
+++ src/lib/components/transaction/NewTransactionForm.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { Transaction } from '$lib/models/Transaction';
+	import moment from 'moment';
+
+	let date = moment().format('x'),
+		amount = 0,
+		price = 0;
+
+	function addTransaction() {
+		const transaction = new Transaction(date, amount, price);
+		console.log(transaction);
+	}
+</script>
+
+<button on:click={addTransaction}>Test</button>
